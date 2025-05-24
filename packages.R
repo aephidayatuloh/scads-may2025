bd73fbe18b5601b7ee2275367f0caaf2ae635ffc
Aep Hidyatuloh <aef.stk@gmail.com>
2025-05-24 18:36:24 +0700

    Add files via upload

---
+++ packages.R
@@ -0,0 +1,4 @@
+install.packages("pak")
+pak::pak(c("tidyverse", "tidymodels", "lme4", "skimr", "ggcorrplot", "performance", "see", 
+           "glmnet", "ranger", "xgboost", "gridExtra", "broom", "ggridges", "vip", 
+           "rpart.plot", "DALEX", "DALEXtra", "modelStudio", "finetune", "themis"))
